6e6df7413dfcfe1dc2896dd637331920613238ae
송민정 <mjm6k.naver.com>
2024-07-23 15:31:07 +0900

    Fix : score 변수 생성

--- src/analysis/AnalysisResult.js
+++ src/analysis/AnalysisResult.js
@@ -10,10 +10,14 @@ from 'react-native';
 import LargeButton from '../components/LargeButton';
 
 const AnalysisResult = ({navigation}) => {
+  const rand = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  const score = rand(0, 101)
   return (
     <View style={styles.container}>
       <Text style={styles.headerText}>분석 결과</Text>
-      <Text>사용자님의 자세는 100점 입니다!</Text>
+      <Text>사용자님의 자세는 {score}점 입니다!</Text>
       <View style={styles.reviewContainer}>
         <Text>자세 리뷰</Text>
         <Text>상체 자세는 </Text>
